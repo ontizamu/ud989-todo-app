606e4d9f66273fab1443f3c4b111c665fe9a60d0
Esther Ontiveros <eontiveros@yahoo.com>
2015-08-30 10:50:15 -0700

    Add priority option

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			'dblclick label': 'edit',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -33,7 +34,6 @@ var app = app || {};
 		initialize: function () {
 			this.listenTo(this.model, 'change', this.render);
 			this.listenTo(this.model, 'destroy', this.remove);
-			this.listenTo(this.model, 'edit-btn', this.edit);
 			this.listenTo(this.model, 'visible', this.toggleVisible);
 		},
 
@@ -52,6 +52,8 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			console.log (this.model.get('priority'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -70,6 +72,13 @@ var app = app || {};
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
+		},
+
+		// Toggle the "priority" state of the model.
+		togglePriority: function () {
+			if (!this.model.get('completed')) {
+				this.model.togglePriority();
+			};
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
